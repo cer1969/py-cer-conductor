bc82050e2f0a6df898906ebbab5cfa74dd64d0ba
Cristian Echeverria <Cristian Echeverria@localhost>
2014-02-17 17:46:51 -0300

    cambio menor

--- test/cxTest.py
+++ test/cxTest.py
@@ -10,3 +10,13 @@ a = cx.Conductor()
 print type(a) is cx.Conductor
 print cx.Conductor
 print cx.version
+
+
+cu300 = cx.Conductor("CU 300 MCM", cx.CC_CU, 15.95, 152.00, 1.378, 6123.0, 0.12270, 0, "")
+cc = cx.CurrentCalc(cu300)
+
+for i, va in enumerate([10, 15, 20, 25, 30]):
+    for j, vc in enumerate([30, 35, 40, 45, 50]):
+        current = cc.getCurrent(va, vc)
+        print "i=%d, i=%d, Ta=%.2f, Tc=%.2f, I=%.2f" % (i, j, va, vc, current)
+        
