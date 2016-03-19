b76a0288e5a7f3e05251808cb75387ebea7c309f
Cristian Echeverría <cer1969@gmail.com>
2016-03-19 01:13:23 -0300

    Correcciones

--- test/currentcalc_test.py
+++ test/currentcalc_test.py
@@ -17,9 +17,9 @@ class TCConstructor(unittest.TestCase):
         cc = cx.CurrentCalc(self.cond)
         
         self.assertEqual(cc.conductor, self.cond)
-        self.assertEqual(cc.altitude, 300.0)
-        self.assertEqual(cc.airVelocity, 2.0)
-        self.assertEqual(cc.sunEffect, 1.0)
+        self.assertEqual(cc.altitude, 300)
+        self.assertEqual(cc.airVelocity, 2)
+        self.assertEqual(cc.sunEffect, 1)
         self.assertEqual(cc.emissivity, 0.5)
         self.assertEqual(cc.formula, cx.CF_IEEE)
         self.assertEqual(cc.deltaTemp, 0.0001)
@@ -30,7 +30,7 @@ class TCConstructor(unittest.TestCase):
     def test_error_r25(self):
         self.cond.r25 = 0.001
         self.assertTrue(cx.CurrentCalc(self.cond))
-        self.cond.r25 = 0.0
+        self.cond.r25 = 0
         self.assertRaises(ValueError, cx.CurrentCalc, self.cond)
         self.cond.r25 = -0.001
         self.assertRaises(ValueError, cx.CurrentCalc, self.cond)
@@ -38,7 +38,7 @@ class TCConstructor(unittest.TestCase):
     def test_error_diameter(self):
         self.cond.diameter = 0.001
         self.assertTrue(cx.CurrentCalc(self.cond))
-        self.cond.diameter = 0.0
+        self.cond.diameter = 0
         self.assertRaises(ValueError, cx.CurrentCalc, self.cond)
         self.cond.diameter = -0.001
         self.assertRaises(ValueError, cx.CurrentCalc, self.cond)
@@ -48,11 +48,11 @@ class TCConstructor(unittest.TestCase):
         self.assertTrue(cx.CurrentCalc(self.cond))
         self.cate.alpha = 0.999
         self.assertTrue(cx.CurrentCalc(self.cond))
-        self.cate.alpha = 0.0
+        self.cate.alpha = 0
         self.assertRaises(ValueError, cx.CurrentCalc, self.cond)
         self.cate.alpha = -0.001
         self.assertRaises(ValueError, cx.CurrentCalc, self.cond)
-        self.cate.alpha = 1.0
+        self.cate.alpha = 1
         self.assertRaises(ValueError, cx.CurrentCalc, self.cond)
         self.cate.alpha = 1.001
         self.assertRaises(ValueError, cx.CurrentCalc, self.cond)
@@ -70,54 +70,51 @@ class TCProperties(unittest.TestCase):
         setattr(self.cc, prop, value)
         return True
     
-    def test_values(self):
-        # Varifica asignación de valores correcta
+    def test_conductor(self):
         self.assertEqual(self.cc.conductor, self.cond)
-        
+        self.assertRaises(AttributeError, self.SetValue, "conductor", 1)
+    
+    def test_altitude(self):
         self.cc.altitude = 150.0
         self.assertEqual(self.cc.altitude, 150.0)
-        
+        self.assertTrue(self.SetValue("altitude", 0))
+        self.assertRaises(ValueError, self.SetValue, "altitude", -0.001)
+    
+    def test_airVelocity(self):
         self.cc.airVelocity = 2.0
         self.assertEqual(self.cc.airVelocity, 2.0)
-        
+        self.assertTrue(self.SetValue("airVelocity", 0))
+        self.assertRaises(ValueError, self.SetValue, "airVelocity", -0.001)
+    
+    def test_sunEffect(self):
         self.cc.sunEffect = 0.5
         self.assertEqual(self.cc.sunEffect, 0.5)
-        
+        self.assertTrue(self.SetValue("sunEffect", 0))
+        self.assertTrue(self.SetValue("sunEffect", 1))
+        self.assertRaises(ValueError, self.SetValue, "sunEffect", -0.001)
+        self.assertRaises(ValueError, self.SetValue, "sunEffect", 1.001)
+    
+    def test_emissivity(self):
         self.cc.emissivity = 0.7
         self.assertEqual(self.cc.emissivity, 0.7)
-        
+        self.assertTrue(self.SetValue("emissivity", 0))
+        self.assertTrue(self.SetValue("emissivity", 1))
+        self.assertRaises(ValueError, self.SetValue, "emissivity", -0.001)
+        self.assertRaises(ValueError, self.SetValue, "emissivity", 1.001)
+    
+    def test_formula(self):
         self.cc.formula = cx.CF_IEEE
         self.assertEqual(self.cc.formula, cx.CF_IEEE)
-        
-        self.cc.deltaTemp = 0.01
-        self.assertEqual(self.cc.deltaTemp, 0.01)
-        
-    def test_errors(self):
-        # Verifica que lanza error con valores fuera de rango
-        self.assertRaises(AttributeError, self.SetValue, "conductor", 1)
-        
-        self.assertTrue(self.SetValue("altitude", 0))
-        self.assertRaises(ValueError, self.SetValue, "altitude", -0.1)
-        
-        self.assertTrue(self.SetValue("airVelocity", 0))
-        self.assertRaises(ValueError, self.SetValue, "airVelocity", -0.1)
-        
-        self.assertTrue(self.SetValue("sunEffect", 0))
-        self.assertRaises(ValueError, self.SetValue, "sunEffect", -0.1)
-        self.assertTrue(self.SetValue("sunEffect", 1))
-        self.assertRaises(ValueError, self.SetValue, "sunEffect", 1.1)
-        
-        self.assertTrue(self.SetValue("emissivity", 0))
-        self.assertRaises(ValueError, self.SetValue, "emissivity", -0.1)
-        self.assertTrue(self.SetValue("emissivity", 1))
-        self.assertRaises(ValueError, self.SetValue, "emissivity", 1.1)
-        
         self.assertTrue(self.SetValue("formula",cx.CF_IEEE))
         self.assertTrue(self.SetValue("formula",cx.CF_CLASSIC))
         self.assertRaises(ValueError, self.SetValue, "formula", "")
+        self.assertRaises(ValueError, self.SetValue, "formula", 1)
         
+    def test_deltaTemp(self):
+        self.cc.deltaTemp = 0.001
+        self.assertEqual(self.cc.deltaTemp, 0.001)
         self.assertTrue(self.SetValue("deltaTemp", 0.0001))
-        self.assertRaises(ValueError, self.SetValue, "deltaTemp", -0.1)
+        self.assertRaises(ValueError, self.SetValue, "deltaTemp", -0.0001)
         self.assertRaises(ValueError, self.SetValue, "deltaTemp", 0)
 
 #-----------------------------------------------------------------------------------------
@@ -132,87 +129,87 @@ class TCMethods(unittest.TestCase):
     def test_getResistance(self):
         self.assertTrue(self.cc.getResistance(cx.TC_MIN))
         self.assertTrue(self.cc.getResistance(cx.TC_MAX))
-        self.assertRaises(ValueError, self.cc.getResistance, cx.TC_MIN - 1)
-        self.assertRaises(ValueError, self.cc.getResistance, cx.TC_MAX + 1)
+        self.assertRaises(ValueError, self.cc.getResistance, cx.TC_MIN - 0.001)
+        self.assertRaises(ValueError, self.cc.getResistance, cx.TC_MAX + 0.001)
     
     def test_getCurrent(self):
-        self.assertRaises(ValueError, self.cc.getCurrent, cx.TA_MIN - 1, 50)
-        self.assertRaises(ValueError, self.cc.getCurrent, cx.TA_MAX + 1, 50)
-        self.assertRaises(ValueError, self.cc.getCurrent, 25, cx.TC_MIN - 1)
-        self.assertRaises(ValueError, self.cc.getCurrent, 25, cx.TC_MAX + 1)
-        
-        # Valores
-        self.assertEqual(self.cc.getCurrent(25.0, 25.0), 0.0)
-        self.assertEqual(self.cc.getCurrent(30.0, 25.0), 0.0)
+        self.assertEqual(self.cc.getCurrent(25, 25), 0)
+        self.assertEqual(self.cc.getCurrent(26, 25), 0)
         
         self.cc.formula = cx.CF_CLASSIC
-        self.assertAlmostEqual(self.cc.getCurrent(25.0, 50.0), 517.7, 1)
-        self.assertAlmostEqual(self.cc.getCurrent(30.0, 60.0), 585.4, 1)
-        self.assertAlmostEqual(self.cc.getCurrent(10.0, 30.0), 438.4, 1)
+        self.assertAlmostEqual(self.cc.getCurrent(25, 50), 517.7, 1)
+        self.assertAlmostEqual(self.cc.getCurrent(30, 60), 585.4, 1)
+        self.assertAlmostEqual(self.cc.getCurrent(10, 30), 438.4, 1)
         
-        amp1 = self.cc.getCurrent(3.0, 30.0)
+        amp1 = self.cc.getCurrent(3, 30)
         self.cc.formula = cx.CF_IEEE
-        amp2 = self.cc.getCurrent(3.0, 30.0)
+        amp2 = self.cc.getCurrent(3, 30)
         self.assertNotEqual(amp1, amp2)
         
-        self.cc.formula = cx.CF_CLASSIC
         self.cc.sunEffect = 1.0
         amp1 = self.cc.getCurrent(25.0, 50.0)
         self.cc.sunEffect = 0.0
         amp2 = self.cc.getCurrent(25.0, 50.0)
         self.assertNotEqual(amp1, amp2)
-
+        
+        self.assertTrue(self.cc.getCurrent(cx.TA_MIN, 50))
+        self.assertTrue(self.cc.getCurrent(cx.TA_MAX, 50)>=0)
+        self.assertTrue(self.cc.getCurrent(25, cx.TC_MIN)>=0)
+        self.assertTrue(self.cc.getCurrent(25, cx.TC_MAX))
+        self.assertRaises(ValueError, self.cc.getCurrent, cx.TA_MIN - 0.001, 50)
+        self.assertRaises(ValueError, self.cc.getCurrent, cx.TA_MAX + 0.001, 50)
+        self.assertRaises(ValueError, self.cc.getCurrent, 25, cx.TC_MIN - 0.001)
+        self.assertRaises(ValueError, self.cc.getCurrent, 25, cx.TC_MAX + 0.001)
+    
     def test_getTc(self):
         # Verifica que los cálculos de getTc sean coherentes con getCurrent
-        amp1 = self.cc.getCurrent(25.0, 50.0)
-        amp2 = self.cc.getCurrent(35.0, 65.0)
-        tc1 = self.cc.getTc(25.0, amp1)
-        tc2 = self.cc.getTc(35.0, amp2)
-        
+        amp1 = self.cc.getCurrent(25, 50)
+        amp2 = self.cc.getCurrent(35, 65)
+        tc1 = self.cc.getTc(25, amp1)
+        tc2 = self.cc.getTc(35, amp2)
         self.assertTrue(abs(tc1 - 50) < self.cc.deltaTemp)
         self.assertTrue(abs(tc2 - 65) < self.cc.deltaTemp)
         
         # Verifica rangos de entrada para ta
         Icmax = self.cc.getCurrent(cx.TA_MIN, cx.TC_MAX)
         self.assertTrue(self.cc.getTc(cx.TA_MIN, Icmax))
-        self.assertRaises(ValueError, self.cc.getTc, cx.TA_MIN - 1, Icmax)
+        self.assertRaises(ValueError, self.cc.getTc, cx.TA_MIN - 0.0001, Icmax)
         
         Icmax = self.cc.getCurrent(cx.TA_MAX, cx.TC_MAX)
         self.assertTrue(self.cc.getTc(cx.TA_MAX, Icmax))
-        self.assertRaises(ValueError, self.cc.getTc, cx.TA_MAX + 1, Icmax)
+        self.assertRaises(ValueError, self.cc.getTc, cx.TA_MAX + 0.0001, Icmax)
         
         # Verifica rangos de entrada para ic
-        self.assertRaises(ValueError, self.cc.getTc, 30, -1)
+        self.assertRaises(ValueError, self.cc.getTc, 30, -0.001)
         Icmax = self.cc.getCurrent(30, cx.TC_MAX)
         self.assertTrue(self.cc.getTc(30, Icmax))
-        self.assertRaises(ValueError, self.cc.getTc, 30, Icmax + 1)
+        self.assertRaises(ValueError, self.cc.getTc, 30, Icmax + 0.001)
     
     def test_getTa(self):
         # Verifica que los cálculos de getTa sean coherentes con getCurrent
-        amp1 = self.cc.getCurrent(25.0, 50.0)
-        amp2 = self.cc.getCurrent(35.0, 65.0)
-        ta1 = self.cc.getTa(50.0, amp1)
-        ta2 = self.cc.getTa(65.0, amp2)
-        
+        amp1 = self.cc.getCurrent(25, 50)
+        amp2 = self.cc.getCurrent(35, 65)
+        ta1 = self.cc.getTa(50, amp1)
+        ta2 = self.cc.getTa(65, amp2)
         self.assertTrue(abs(ta1 - 25) < self.cc.deltaTemp)
         self.assertTrue(abs(ta2 - 35) < self.cc.deltaTemp)
         
         # Verifica rangos de entrada para tc
         self.assertTrue(self.cc.getTa(cx.TC_MIN, 0))
-        self.assertRaises(ValueError, self.cc.getTa, cx.TC_MIN - 1, 0)
+        self.assertRaises(ValueError, self.cc.getTa, cx.TC_MIN - 0.0001, 0)
         
         Icmax = self.cc.getCurrent(cx.TA_MIN, cx.TC_MAX)
         self.assertTrue(self.cc.getTa(cx.TC_MAX, Icmax))
-        self.assertRaises(ValueError, self.cc.getTa, cx.TC_MAX + 1, Icmax)
+        self.assertRaises(ValueError, self.cc.getTa, cx.TC_MAX + 0.0001, Icmax)
         
         # Verifica rangos de entrada para ic
         Icmin = self.cc.getCurrent(cx.TA_MAX, 100)
         Icmax = self.cc.getCurrent(cx.TA_MIN, 100)
         
         self.assertTrue(self.cc.getTa(100, Icmin))
-        self.assertRaises(ValueError, self.cc.getTa, 100, Icmin - 1)
+        self.assertRaises(ValueError, self.cc.getTa, 100, Icmin - 0.0001)
         self.assertTrue(self.cc.getTa(100, Icmax))
-        self.assertRaises(ValueError, self.cc.getTa, 100, Icmax + 1)
+        self.assertRaises(ValueError, self.cc.getTa, 100, Icmax + 0.0001)
         
 #-----------------------------------------------------------------------------------------
 
