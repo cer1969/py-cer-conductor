cd533eac86407c1243326de1e96f2a16ea5dae13
Cristian Echeverr?a <cer1969@gmail.com>
2016-12-31 22:01:56 -0300

    pruebas

--- test_zx/test01.py
+++ test_zx/test01.py
@@ -1,6 +1,6 @@
-from cer.conductor import _zx
+from cer.conductor import zx
 
-catmk = _zx.CategoryMaker('COPPER', 12000.0, 0.0000169, 0.0, 0.00374)
+catmk = zx.CategoryMaker('COPPER', 12000.0, 0.0000169, 0.0, 0.00374)
 
 cat1 = catmk.get()
 catmk.name = "Juan"
@@ -9,4 +9,9 @@ cat2 = catmk.get()
 print(cat1.name)
 print(cat2.name)
 
-print(_zx.CC_AAAC.name)
+print(zx.CC_AAAC.name)
+
+condmk = zx.ConductorMaker("AAAC 740,8 MCM FLINT", zx.CC_AAAC, diameter=25.17, r25=0.089360)
+c1 = condmk.get()
+
+print(c1)
