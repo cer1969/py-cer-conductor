f8a4bb804368b3afdf84393ea01b268cb5b7f53d
Cristian Echeverr?a <cer1969@gmail.com>
2017-01-06 11:04:12 -0300

    Compilaci?n con MSVC

--- make.cmd
+++ make.cmd
@@ -2,22 +2,22 @@
 @echo off
 setlocal
 
-if not defined PYTHON_DEV goto :err_python_dev
+if not defined PYTHON_ROOT goto :err_python_root
 
 set module=_zx
 
 if not "%~1"=="" goto :%~1
 goto :build
 
-:err_python_dev
-set err=ERROR: PYTHON_DEV NO DEFINIDA
+:err_python_root
+set err=ERROR: PYTHON_ROOT NO DEFINIDA
 goto :end
 
 :build
 echo Building %module%.pyd
 cython -a -o ztmp\%module%.c %module%.pyx
-gcc -c -DMS_WIN64 -Ofast -I%PYTHON_DEV%\include -o ztmp\%module%.o ztmp\%module%.c
-gcc -shared -L%PYTHON_DEV%\libs -o %module%.pyd ztmp\%module%.o -lpython35
+gcc -c -DMS_WIN64 -Ofast -I%PYTHON_ROOT%\include -o ztmp\%module%.o ztmp\%module%.c
+gcc -shared -L%PYTHON_ROOT%\libs -o %module%.pyd ztmp\%module%.o -lpython35
 goto :end
 
 :clean
