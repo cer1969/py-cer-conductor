38032fed742fd9538c33f2acd619ae31575d7a85
Cristian Echeverría <cer1969@gmail.com>
2017-01-06 11:04:12 -0300

    Compilaci?n con MSVC

---
+++ wp-3.5.2.3-consola.cmd
@@ -0,0 +1,9 @@
+@echo off
+COLOR 1E
+set PYTHON_ROOT=C:\Users\crecheverria\Apps\WinPython\3.5.2.3Zero\python-3.5.2.amd64
+set MINGW_ROOT=C:\Users\crecheverria\Apps\mingw
+set SCITE_ROOT=C:\Users\crecheverria\Apps\IDEs\wscite
+SET CODE_ROOT=C:\Users\crecheverria\Apps\IDEs\VSCode
+
+SET PATH=%PYTHON_ROOT%;%PYTHON_ROOT%\Scripts;%MINGW_ROOT%\bin;%SCITE_ROOT%;%CODE_ROOT%;%PATH%
+cmd
