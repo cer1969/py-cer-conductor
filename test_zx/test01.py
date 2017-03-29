b36d24d03fe7251a874b4a847fef9e5da686e298
Cristian Echeverría <cer1969@gmail.com>
2017-03-29 20:23:56 -0300

    Mejoras en CategoryMaker y ConductorMaker en _zx.pyx

--- test_zx/test01.py
+++ test_zx/test01.py
@@ -11,7 +11,8 @@ print(cat2.name)
 
 print(zx.CC_AAAC.name)
 
-condmk = zx.ConductorMaker("AAAC 740,8 MCM FLINT", zx.CC_AAAC, diameter=25.17, r25=0.089360)
+catmk2 = zx.CategoryMaker.fromCategory(zx.CC_AAAC)
+condmk = zx.ConductorMaker("AAAC 740,8 MCM FLINT", catmk2, diameter=25.17, r25=0.089360)
 c1 = condmk.get()
 
 print(c1)
