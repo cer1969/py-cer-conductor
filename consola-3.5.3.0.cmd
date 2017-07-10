62b2dbf2bdb4453ec33b4705237052b15b7ab9e4
Cristian Echeverría <cer1969@gmail.com>
2017-07-10 11:04:49 -0400

    Ajustes para varias versiones de python

--- consola-3.5.3.0.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-COLOR 1E
-set PYTHON_ROOT=C:\Users\crecheverria\Apps\WinPython\3.5.3.0Zero\python-3.5.3.amd64
-set MINGW_ROOT=C:\Users\crecheverria\Apps\mingw
-set SCITE_ROOT=C:\Users\crecheverria\Apps\IDEs\wscite
-SET CODE_ROOT=C:\Users\crecheverria\Apps\IDEs\VSCode
-
-SET PATH=%PYTHON_ROOT%;%PYTHON_ROOT%\Scripts;%MINGW_ROOT%\bin;%SCITE_ROOT%;%CODE_ROOT%;%PATH%
-cmd
