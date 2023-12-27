977f1b7284660e9cca9b05ee595c76dd6d1b2646
cer1969 <cer1969@gmail.com>
2023-12-27 02:58:36 -0300

    test header file

---
+++ zxsrc/constants.h
@@ -0,0 +1,11 @@
+//----------------------------------------------------------------------------------------
+// Constants
+
+#define _CF_IEEE 0
+#define _CF_CLASSIC 1
+#define _TA_MIN -90.0
+#define _TA_MAX 90.0
+#define _TC_MIN -90.0
+#define _TC_MAX 2000.0
+#define _TENSION_MAX 50000
+//#define _ITER_MAX 20000
